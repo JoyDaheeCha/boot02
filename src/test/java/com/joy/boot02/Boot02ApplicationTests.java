9ae4a92da85ae64d914de838a9310acb8231b71f
JoyDaheeCha <daheecha0612@gmail.com>
2021-05-08 20:24:57 +0900

    Initial commit

---
+++ src/test/java/com/joy/boot02/Boot02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.boot02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
